434141ceb7c86caeec54b8ac0d590ce1d959bccd
jhamadhav <jhamadhav28@gmail.com>
2023-10-29 19:45:39 +0530

    query feature removed from authority portal

--- authority-portal/public/scripts/main2.js
+++ authority-portal/public/scripts/main2.js
@@ -171,21 +171,6 @@ const attachEvent = (yourApplications) => {
    }
 };
 
-async function query(data) {
-   const response = await fetch(
-      "https://api-inference.huggingface.co/models/mrm8488/bert-tiny-finetuned-sms-spam-detection",
-      {
-         headers: {
-            Authorization: "Bearer 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",
-         },
-         method: "POST",
-         body: JSON.stringify(data),
-      }
-   );
-   const result = await response.json();
-   return result;
-}
-
 const fillView = async (i) => {
    document.getElementById("subject-out").innerText =
       yourApplications[i]["subject"];
